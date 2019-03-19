3466cb96bff5d04b9446e3f765bd6f287361a928
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-03-19 15:28:02 -0500

    setup hot module reloading

--- src/index.js
+++ src/index.js
@@ -23,3 +23,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+	module.hot.accept('./print.js', function() {
+		console.log('accepting');
+		printMe();
+	})
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
   const path = require('path');
   const HtmlWebpackPlugin = require('html-webpack-plugin');
   const CleanWebpackPlugin = require('clean-webpack-plugin');
+  const webpack = require('webpack');
 
   module.exports = {
   	mode: 'development',
     entry: {
-    	app: './src/index.js',
-    	print: './src/print.js'
+    	app: './src/index.js'
     }, 
     devtool: 'inline-source-map',
     devServer: {
@@ -16,7 +16,8 @@
     	new CleanWebpackPlugin(),
     	new HtmlWebpackPlugin({
     		title: 'Output Management'
-    	})
+    	}),
+    	new webpack.HotModuleReplacementPlugin()
     ],
     output: {
 	  filename: '[name].bundle.js',
