a8ae53a55431be56832ddd2ec638e676d7b0e471
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-03-19 13:43:05 -0500

    setup css img and font loaders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+ import './style.css';
+ import Icon from './icon.png';
 
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
